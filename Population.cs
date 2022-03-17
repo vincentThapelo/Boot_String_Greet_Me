90e0e796ad3a54959b1f0bbe2e561d0e8a682d12
vincentThapelo <vthapelo@gmail>
2022-03-17 13:51:53 -0700

    commiting files

---
+++ Population.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopulationApp
+{
+    class Population
+    {
+
+        public String CountryName { get; set; }
+        public int[] Year = new int[2];
+        public int[] Populations = new int[100];
+        public int Average { get; }
+        public int Highest { get; }
+        public int Lowest { get; }
+
+        public int CalcAverage()
+        {
+            int average = 0;
+
+            for (int x = 0; x < Year.Length; x++)
+            {
+
+                average = average + Populations[x];
+
+            }
+            average = average / Year.Length ;
+
+            return average;
+        }
+
+        public void HighestPopulation()
+        {
+            int Highest1 = 0;
+            int Highest = 0;
+            int value = 0;
+
+            for (int y = 0; y < Year.Length; y++)
+            {
+
+                if (Populations[y] > Highest)
+                {
+
+                    Highest = Populations[y];
+                    value = y;
+                }
+
+            }
+            Highest1 = Highest;
+            Console.WriteLine("Year with highest population is :" + Year[value] + " with a Populate of " + Highest1);
+        }
+
+
+        public void LowestPopulation()
+        {
+
+            int Lowest1 = 0;
+            int Lowest = 0;
+            int index = 0;
+            int Point = 0;
+            for (int x = 0; x < Year.Length; x++)
+            {
+                if (Populations[x] < Lowest)
+                {
+
+                    Lowest = Populations[x];
+                    index = x;
+                }
+
+            }
+            Lowest1 = Lowest;
+            Point = index;
+            Console.WriteLine("Year with lowest population is :" + Year[Point] + " with a Population of " + Lowest1);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ PopulationApp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopulationApp
+{
+    class PopulationApp
+    {
+        static void Main(string[] args)
+        {
+
+            Population Population = new Population();
+            int[] Year = new int[2];
+            int[] Populations = new int[2];
+            Console.WriteLine("Please Enter the country name :");
+            Population.CountryName = Console.ReadLine();
+            Console.Write("Please Enter the Start year : ");
+            Year[0] = Convert.ToInt32(Console.ReadLine());
+            Population.Year[0] = Year[0];
+
+            int count = 2;
+
+            for (int x = 1; x < Year.Length; x++)
+            {
+
+                //count = count + 1;
+                Console.Write("Please Enter year " + count + " : ");
+                Year[x] = Convert.ToInt32(Console.ReadLine());
+                Population.Year[x] = Year[x];
+
+                count = count + 1;
+
+            }
+            Console.WriteLine("Please Enter start year Population :");
+            Populations[0] = Convert.ToInt32(Console.ReadLine());
+            Population.Populations[0] = Populations[0];
+
+            int counter1 = 2;
+            for (int y = 1; y < Year.Length; y++)
+            {
+
+
+
+                Console.WriteLine("Please Enter year " + counter1 + " Population :");
+                Populations[y] = Convert.ToInt32(Console.ReadLine());
+                Population.Populations[y] = Populations[y];
+
+                counter1 = counter1 + 1;
+
+            }
+
+            Console.WriteLine("Country Name : " + Population.CountryName);
+            Console.WriteLine("Year \tPopulation");
+            for (int w = 0; w < Year.Length; w++)
+            {
+                
+
+                Console.WriteLine(Year[w] + "\t " + Populations[w]);
+                
+            }
+            Console.WriteLine("The average population Between the years is " + Population.CalcAverage());
+
+            Population.HighestPopulation();
+            Population.LowestPopulation();
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
